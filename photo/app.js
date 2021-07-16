bc86676663b2adb20a44b75651189842124af61c
JaeYoon <ljayoon@gmail.com>
2021-07-17 01:22:58 +0900

    사진정리

--- photo/app.js
+++ photo/app.js
@@ -3,11 +3,66 @@ const os = require("os");
 const path = require("path");
 
 const folder = process.argv[2];
+// console.log(folder);
 
 const workingDir = path.join(os.homedir(), "Pictures", folder);
-if (!folder || fs.existsSync(workingDir)) {
+if (!folder || !fs.existsSync(workingDir)) {
   console.error("please enter folder name in Pictures");
   return;
 }
 
-console.log(workingDir);
+// console.log(workingDir);
+
+const videoDir = path.join(workingDir, "video");
+const capturedDir = path.join(workingDir, "captured");
+const duplicatedDir = path.join(workingDir, "duplicated");
+
+// console.log(videoDir);
+// console.log(capureDir);
+// console.log(duplicateDir);
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+fs.promises.readdir(workingDir).then(processFiles).catch(console.log);
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      move(file, videoDir);
+    } else if (isCapturedFile(file)) {
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  //   console.log(match);
+  return !!match;
+}
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  //   console.log(match);
+  return !!match;
+}
+function isDuplicatedFile(files, file) {
+  if (!file.startsWith("IMG_") || file.startsWith("IMG_E")) {
+    return false;
+  }
+  const edited = `IMG_E${file.split("_")[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  console.info(`move ${file} to ${path.basename(targetDir)}`);
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises.rename(oldPath, newPath).catch(console.error);
+}
