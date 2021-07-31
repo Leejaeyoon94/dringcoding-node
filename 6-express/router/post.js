d6173bdf1a29608ab15cd2abfe26340324545544
JaeYoon <ljayoon@gmail.com>
2021-08-01 03:20:29 +0900

    end

--- 6-express/router/post.js
+++ 6-express/router/post.js
@@ -1,21 +1,21 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(201).send('GET: /posts');
+router.get("/", (req, res) => {
+  res.status(201).send("GET: /posts");
 });
 
-router.post('/', (req, res) => {
-  res.status(201).send('POST: /posts');
+router.post("/", (req, res) => {
+  res.status(201).send("POST: /posts");
 });
 
-router.put('/:id', (req, res) => {
-  res.status(201).send('PUT: /posts/:id');
+router.put("/:id", (req, res) => {
+  res.status(201).send("PUT: /posts/:id");
 });
 
-router.delete('/:id', (req, res) => {
-  res.status(201).send('DELETE: /posts/:id');
+router.delete("/:id", (req, res) => {
+  res.status(201).send("DELETE: /posts/:id");
 });
 
 export default router;
