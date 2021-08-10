38c4eb7a762a847fce716f46480988c5a84451b8
Leejaeyoon94 <67680543+Leejaeyoon94@users.noreply.github.com>
2021-08-10 22:54:21 +0900

    mysql

--- 12-dwitter-tweets/server/app.js
+++ 12-dwitter-tweets/server/app.js
@@ -19,6 +19,8 @@ import dotenv from "dotenv";
 import { config } from "./config.js";
 import { Server } from "socket.io";
 import { initSocket } from "./connection/socket.js";
+import { db } from "./db/database.js";
+
 dotenv.config();
 
 app.use((req, res, next) => {
@@ -29,5 +31,8 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
+
+db.getConnection().then();
+
 const server = app.listen(config.host.port);
 initSocket(server);
